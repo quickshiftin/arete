d37a6ae94d8a4b03c24e0593f69a5163d4f8cd19
Nathan Nobbe <nathan@moxune.com>
2014-02-24 17:52:03 -0700

    Initial commit

---
+++ arete-lib.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#------------------------------------------------------------
+# The *crude* beginnings of what might become a BASH
+# multi-host command executor, similar to Fabric in Python.
+#------------------------------------------------------------
+
+# scp wrapper for arete
+function arete_put
+{
+    echo ''
+}
+
+# Async mode, spit out the hostname from each box
+# Async mode, spit out the hostname from each box
+function arete_run
+{
+    local command="$1"
+
+    # Loop over the commands running them on the target boxes
+    for host in "${ARETE_HOSTS[@]}"; do
+        if [ $ARETE_ASYNC -eq 1 ]; then
+            ( ssh nathan@"$host"  "bash -c '$command'") &
+        else
+            ssh nathan@"$host" "bash -c '$command'"
+        fi
+    done
+
+    # Wait for the background jobs in async mode
+    if [ $ARETE_ASYNC -eq 1 ]; then
+        wait
+    fi
+}
+
+function arete_append_host
+{
+    # Create with initial element
+    if [ -z "$ARETE_HOSTS" -o "${#ARETE_HOSTS[@]}" -eq 0 ]; then
+        ARETE_HOSTS=("$1")
+
+    # Append to the array
+    else
+        # Check the array to see if the host is already present
+        case "${ARETE_HOSTS[@]}" in  *"$1"*) return 0 ;; esac
+
+        ARETE_HOSTS+=("$1")
+    fi
+}
+
+function arete_hosts
+{
+    local a=
+    local is_array=
+    local arg=
+    local inner_arg=
+
+    # Loop over args
+    for arg in $(seq $#); do
+        # If argument is not an array, simply append it
+        a="${!arg}"
+        is_array=$(declare -p a 2> /dev/null | grep -q '^declare \-a' | wc -l)
+        if [ "$is_array" -eq 1 ]; then
+            for inner_arg in ${a[@]}; do
+                arete_append_host $a["$inner_arg"]
+            done
+        else
+            arete_append_host "$a"
+        fi
+    done
+}
+
+function arete
+{
+    local task="task_${1}"
+
+    # For now arguments are any extra params, but at some point
+    # let's take in args like fabric does :-delimited so hosts
+    # can be specified on the fly
+    "$task"
+}
