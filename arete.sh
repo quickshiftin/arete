0bd0a36714df0bc67817ad1f36477cf6295e28fc
Nathan Nobbe <nathan@moxune.com>
2014-03-18 20:24:21 -0600

    A ton of work and an ignore file.

--- arete.sh
+++ arete.sh
@@ -4,19 +4,54 @@
 # @note May package this in a single file ...
 source ./arete-lib.sh
 
-_usage(){
-  egrep "function task_|^[^_].\+(){$" $1 | while read line; do
-    local cmd=$(echo "$line" | sed "s/(){//g" | sed 's/function task_//g')
-    local info=$(grep -C0 -A0 -B1 "function task_$cmd" $1 | sed "N;s/\n.*//g")
-    printf "    %-20s %-40s\n" "$cmd" "$info" | grep "#"
-  done; echo "";
+function _usage
+{
+    # If the invocation to arete was total garbage...
+    if [ -z "$1" ]; then
+        echo "$0" '[-s]' '<task>[:arg1,..argx]' '[host1 hostX]'
+        return
+    fi
+
+    # If there's an 'arete file' to analyze, then do it
+    egrep "function task_|^[^_].\+(){$" $1 | while read line; do
+        # Tasks must be declared as function task_<task name>
+        local cmd=$(echo "$line" | sed "s/(){//g" | sed 's/function task_//g')
+
+        # Look for comments on top of the task, the last line of comments will
+        # be used for the listing output
+        local info=$(grep -C0 -A0 -B1 "function task_$cmd" $1 | sed "N;s/\n.*//g")
+
+        # Print the name of the task and optionally a comment
+        if [ -z "$info" ]; then
+            printf "    %-20s\n" "$cmd"
+        else
+            printf "    %-20s %-40s\n" "$cmd" "$info" | grep "#"
+        fi
+    done; echo "";
 }
+
+# Process the arguments
+ARETE_ASYNC=0
+while getopts "ah" OPTION; do
+    case "$OPTION" in
+        a)
+            ARETE_ASYNC=1
+            ;;
+        h)
+            _usage
+            exit 0
+        #?)
+        #    _usage
+        #    exit 1
+        #    ;;
+    esac
+done
 
 # Bail if functions file is not a regular file
 if [ ! -f "$1" ]; then
     echo "$1" is not a regular file.
     _usage
-    exit
+    exit 1
 fi
 
 # Load the functions file and run the task
@@ -27,4 +62,5 @@ if [ $# -gt 1 ]; then
 # Show usage information
 else
     _usage "$1"
+    exit 1
 fi
